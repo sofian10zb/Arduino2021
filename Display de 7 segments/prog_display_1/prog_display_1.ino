59e622a574a2e6291ff4fb2d27b806540d06f01d
Sofian10zb <sofianzeriouhibachaoui@gmail.com>
2021-04-02 17:53:51 +0200

    Update prog_display_1.ino

--- Display de 7 segments/prog_display_1/prog_display_1.ino
+++ Display de 7 segments/prog_display_1/prog_display_1.ino
@@ -1,6 +1,6 @@
 /**********************************************************************************
 **                                                                               **
-**                              Array de 8 LEDs                                  **
+**                              DISPLAY 7 SEGMENTS                               **
 **                                                                               **
 **                                                                               **
 **********************************************************************************/
@@ -9,26 +9,26 @@
 
 //********** Variables ************************************************************
 
-const int ledA = 5;          // donar nom al pin 7 de l’Arduino
-const int ledB = 6;          // donar nom al pin 8 de l’Arduino
-const int ledC = 7;          // donar nom al pin 9 de l’Arduino
-const int ledD = 8;         // donar nom al pin 10 de l’Arduino
-const int ledE = 9;         // donar nom al pin 11 de l’Arduino
-const int ledF = 10;         // donar nom al pin 12 de l’Arduino
-const int ledG = 11;         // donar nom al pin 12 de l’Arduino
+const int ledA = 5;       
+const int ledB = 6;          
+const int ledC = 7;          
+const int ledD = 8;     
+const int ledE = 9;    
+const int ledF = 10;     
+const int ledG = 11;        
 const int temps = 1000;
 
 
 //********** Setup ****************************************************************
 void setup()
 {
-  pinMode(ledA, OUTPUT);     // definir el pin 7 com una sortida
-  pinMode(ledB, OUTPUT);     // definir el pin 8 com una sortida
-  pinMode(ledC, OUTPUT);     // definir el pin 9 com una sortida
-  pinMode(ledD, OUTPUT);     // definir el pin 10 com una sortida
-  pinMode(ledE, OUTPUT);     // definir el pin 11 com una sortida
-  pinMode(ledF, OUTPUT);     // definir el pin 11 com una sortida
-  pinMode(ledG, OUTPUT);     // definir el pin 12 com una sortida
+  pinMode(ledA, OUTPUT);     
+  pinMode(ledB, OUTPUT);     
+  pinMode(ledC, OUTPUT);   
+  pinMode(ledD, OUTPUT);    
+  pinMode(ledE, OUTPUT); 
+  pinMode(ledF, OUTPUT);   
+  pinMode(ledG, OUTPUT);   
 }
 
 //********** Loop *****************************************************************
